597634aa0a310a66bd57eaf051962a3d7c31ea07
Eva Kratěnová <115078892+Ewka0206@users.noreply.github.com>
2026-01-23 00:35:55 +0100

    ScrollToTop

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header.tsx";
 import { Footer } from "./Footer.tsx";
+import { ScrollToTop } from "../components/ScrollToTop";
 
 export function MainLayout() {
     return (
         <div className="appShell">
             <Header />
+            <ScrollToTop />
             <main className="main">
                 <Outlet />
             </main>
